a67b92e654ba8d8c0caf303bc2013e41ef75740d
smudap <smudap@student.mini.pw.edu.pl>
2017-06-12 19:30:52 +0200

    stopwords update

--- create_stopwords.R
+++ create_stopwords.R
@@ -1,12 +1,12 @@
-
 stopwords <- c(
    "miękka", "miekka", "twarda", "okładka", "okladka", "oprawa", "op",
-   "książka", "ksiazka", "książki", "ksiazki", "książek", "ksiazek",
+   "książka", "ksiazka", "książki", "ksiazki", "książek", "ksiazek", "fantasy",
    paste0("t", 1:100), "tom", "tomy", paste0("tom", 1:100), paste0("t.", 1:100),
    10:3000, paste0(1, "-", 2:100), paste0(2:100, "x"),
    "ksiąg", "ksiag", paste0(2:100, "ksiąg"), paste0(2:100, "ksiag"),
-   "wysyłka", "wysylka", "24h", "nowa", "nowe", "pakiet"
+   "wysyłka", "wysylka", "24h", "nowa", "nowe", "pakiet",
+   "stan", "db", "db+", "bdb", "bdb+", "hit"
 )
-stopwords <- data.frame(stopwords = stopwords)
+stopwords <- data.frame(stopwords = stopwords, stringsAsFactors = FALSE)
 
 write.csv(stopwords, "stopwords.csv", row.names = FALSE, fileEncoding = "UTF-8")
